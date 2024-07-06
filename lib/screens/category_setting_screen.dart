d9ca99706e4ace32fe12f4a7f3d7b15f4302e9c6
dylangoh235 <quanta4578@gmail.com>
2024-07-06 15:19:28 +0900

    style: Change category setting UI

--- lib/screens/category_setting_screen.dart
+++ lib/screens/category_setting_screen.dart
@@ -11,54 +11,185 @@ class CategorySettingScreen extends StatefulWidget {
 }
 
 class _CategorySettingScreenState extends State<CategorySettingScreen> {
-  final items = [
-    {'leftString': 'Item 1', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
-    {'leftString': 'Item 2', 'rightString': '₩ 1000'},
+  final List<ExpenseCategory> expenseCategories = [
+    ExpenseCategory(
+      category: '식비',
+      items: [
+        CategoryItem(description: '프랭크버거', amount: '10000원'),
+        CategoryItem(description: '푸행쿠버거', amount: '10000원'),
+        CategoryItem(description: '마마터치', amount: '10000원'),
+      ],
+    ),
+    ExpenseCategory(
+      category: '생활',
+      items: [
+        CategoryItem(description: '샴푸', amount: '5000원'),
+        CategoryItem(description: '세제', amount: '3000원'),
+      ],
+    ),
+    ExpenseCategory(
+      category: '고정',
+      items: [
+        CategoryItem(description: '월세', amount: '500000원'),
+        CategoryItem(description: '인터넷', amount: '30000원'),
+      ],
+    ),
   ];
-  void _handlePressed(int index, String leftString) {
-    print('Index: $index, Left String: $leftString');
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Profit Note"),
-      ),
+      appBar: AppBar(title: const Text("Profit Note")),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         foregroundColor: ColorTheme.cardLabelText,
         backgroundColor: ColorTheme.backgroundOfBackground,
         child: const Icon(Icons.add),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ControlBtnGroup(
-            leftString: "완료",
-            rightString: "취소",
-            leftCallback: () {},
-            rightCallback: () {},
-          ),
-          Expanded(
-            child: CategoryItemsWidget(
-              items: items,
-              onPressed: _handlePressed,
+      body: Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ControlBtnGroup(
+              titleList: const ["수입", "지출"],
+              callbackList: [() {}, () {}],
             ),
-          )
-        ],
+            Expanded(
+              child: CategoryList(
+                expenseCategories: expenseCategories,
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class ExpenseCategory {
+  final String category;
+  final List<CategoryItem> items;
+
+  ExpenseCategory({required this.category, required this.items});
+}
+
+class CategoryList extends StatelessWidget {
+  final List<ExpenseCategory> expenseCategories;
+
+  const CategoryList({super.key, required this.expenseCategories});
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: expenseCategories.map((category) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Table(
+              columnWidths: const {
+                0: FlexColumnWidth(1),
+                1: FlexColumnWidth(5),
+                2: FlexColumnWidth(3),
+              },
+              children: [
+                TableRow(
+                  children: [
+                    const SizedBox(width: 1),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 16,
+                        top: 6,
+                        bottom: 6,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: TextField(
+                          controller:
+                              TextEditingController(text: category.category),
+                          style: Theme.of(context).textTheme.titleMedium,
+                          textAlign: TextAlign.left,
+                          decoration:
+                              const InputDecoration(border: InputBorder.none),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 8,
+                        top: 6,
+                        bottom: 6,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.redAccent,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Table(
+              columnWidths: const {
+                0: FlexColumnWidth(1),
+                1: FlexColumnWidth(2),
+                2: FlexColumnWidth(3),
+              },
+              children: category.items.map((item) {
+                return TableRow(
+                  children: [
+                    const SizedBox(width: 1),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 8,
+                        top: 6,
+                        bottom: 6,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: TextField(
+                          controller:
+                              TextEditingController(text: item.description),
+                          style: Theme.of(context).textTheme.bodyMedium,
+                          textAlign: TextAlign.left,
+                          decoration:
+                              const InputDecoration(border: InputBorder.none),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 8,
+                        top: 6,
+                        bottom: 6,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.redAccent,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+            const Divider(),
+          ],
+        );
+      }).toList(),
     );
   }
 }
