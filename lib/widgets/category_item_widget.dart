ecb4d4773c8308610c3e0700e19b6447c09f5e9e
Bouncyball <80045020+dylangoh235@users.noreply.github.com>
2024-07-07 21:54:33 +0900

    style: Add Category Setting screen (#38)

--- lib/widgets/category_item_widget.dart
+++ lib/widgets/category_item_widget.dart
@@ -1,46 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:profitnote/style/theme.dart';
 
-// class CategoryItemsWidget extends StatelessWidget {
-//   const CategoryItemsWidget(
-//       {super.key, required this.items, required this.onPressed});
-//   final List<Map<String, String>> items;
-//   final Function(int index, String leftString) onPressed;
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: items.length,
-//       itemBuilder: (BuildContext ctx, int idx) {
-//         final data = items[idx];
-//         return ElevatedButton(
-//           onPressed: () => onPressed(idx, data['leftString']!),
-//           style: ElevatedButton.styleFrom(
-//             backgroundColor: ColorTheme.cardBackground,
-//             shape: const RoundedRectangleBorder(
-//               borderRadius: BorderRadius.zero,
-//             ),
-//             tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-//           ),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//             children: [
-//               Text(
-//                 data['leftString']!,
-//                 style: Theme.of(context).textTheme.titleMedium,
-//               ),
-//               const SizedBox(width: 32),
-//               Text(
-//                 data['rightString']!,
-//                 style: Theme.of(context).textTheme.titleMedium,
-//               ),
-//             ],
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
 class CategoryItem {
   final String description;
   final String amount;
@@ -74,12 +34,12 @@ class CategoryItemsWidget extends StatelessWidget {
             children: [
               Text(
                 data.description,
-                style: Theme.of(context).textTheme.titleMedium,
+                style: Theme.of(context).textTheme.bodyMedium,
               ),
               const SizedBox(width: 32),
               Text(
                 data.amount,
-                style: Theme.of(context).textTheme.titleMedium,
+                style: Theme.of(context).textTheme.bodyMedium,
               ),
             ],
           ),
