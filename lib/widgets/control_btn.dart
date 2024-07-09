10b76ccff209536ff722d3ae6bd6cb58038818c4
Jaehoon Song <jhssong02@gmail.com>
2024-07-09 14:05:28 +0900

    chore: Change style related to ControlBtn (#44)

--- lib/widgets/control_btn.dart
+++ lib/widgets/control_btn.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:profitnote/style/theme.dart';
 
 class ControlBtn extends StatelessWidget {
-  const ControlBtn({super.key, required this.onPressed});
+  const ControlBtn({
+    super.key,
+    this.text = "저장",
+    required this.onPressed,
+  });
+
+  final String text;
 
   final Function() onPressed;
 
@@ -10,20 +16,18 @@ class ControlBtn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
-      width: 100,
-      height: 40,
       child: TextButton(
         onPressed: onPressed,
         style: TextButton.styleFrom(
           backgroundColor: ColorTheme.cardBackground,
-          padding: const EdgeInsets.all(0),
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(20)),
           ),
           foregroundColor: ColorTheme.pointText,
           textStyle: Theme.of(context).textTheme.titleMedium,
         ),
-        child: const Text('저장'),
+        child: Text(text),
       ),
     );
   }
