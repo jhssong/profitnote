baa970b61bdc766a273ec8384f21b3e29f56b0da
Bouncyball <80045020+dylangoh235@users.noreply.github.com>
2024-07-11 22:03:06 +0900

    style: Add CalculatorScreen (#51)

--- lib/widgets/custom_keyboard.dart
+++ lib/widgets/custom_keyboard.dart
@@ -23,15 +23,12 @@ class _KeyboardKeyState extends State<KeyboardKey> {
       return widget.label;
     }
 
-    return TextButton(
-      onPressed: () {},
-      child: Text(
-        widget.label,
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: ColorTheme.cardText,
-        ),
+    return Text(
+      widget.label,
+      style: TextStyle(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        color: ColorTheme.cardText,
       ),
     );
   }
