60afb9e779809e835cc8a895e8aaa971c938dbbf
Bouncyball <80045020+dylangoh235@users.noreply.github.com>
2024-07-14 22:24:37 +0900

    fix: Change CategorySettingScreen UI (#63)

--- lib/screens/setting/category_setting_screen.dart
+++ lib/screens/setting/category_setting_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:profitnote/style/theme.dart';
-import 'package:profitnote/screens/setting/widgets/category_item_widget.dart';
-import 'package:profitnote/widgets/control_btn_group.dart';
+import 'package:profitnote/screens/setting/widgets/category_add_widget.dart';
 
 class CategorySettingScreen extends StatefulWidget {
   const CategorySettingScreen({super.key});
@@ -10,8 +9,9 @@ class CategorySettingScreen extends StatefulWidget {
   State<CategorySettingScreen> createState() => _CategorySettingScreenState();
 }
 
-class _CategorySettingScreenState extends State<CategorySettingScreen> {
-  final List<ExpenseCategory> expenseCategories = [
+class _CategorySettingScreenState extends State<CategorySettingScreen>
+    with SingleTickerProviderStateMixin {
+  final List<ExpenseCategory> _expenseCategories = [
     ExpenseCategory(
       category: '식비',
       items: [
@@ -36,31 +36,113 @@ class _CategorySettingScreenState extends State<CategorySettingScreen> {
     ),
   ];
 
+  void _reorderCategories(int oldIndex, int newIndex) {
+    setState(() {
+      if (newIndex > oldIndex) newIndex -= 1;
+      final movedCategory = _expenseCategories.removeAt(oldIndex);
+      _expenseCategories.insert(newIndex, movedCategory);
+    });
+  }
+
+  void _reorderItems(int categoryIndex, int oldIndex, int newIndex) {
+    setState(() {
+      if (newIndex > oldIndex) newIndex -= 1;
+      final movedItem =
+          _expenseCategories[categoryIndex].items.removeAt(oldIndex);
+      _expenseCategories[categoryIndex].items.insert(newIndex, movedItem);
+    });
+  }
+
+  void _deleteCategory(int index) {
+    setState(() {
+      _expenseCategories.removeAt(index);
+    });
+  }
+
+  void _deleteItem(int categoryIndex, int itemIndex) {
+    setState(() {
+      _expenseCategories[categoryIndex].items.removeAt(itemIndex);
+    });
+  }
+
+  final List<Widget> _tabs = [
+    const Tab(text: "수입"),
+    const Tab(text: "지출"),
+  ];
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Profit Note")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        foregroundColor: ColorTheme.cardLabelText,
-        backgroundColor: ColorTheme.backgroundOfBackground,
-        child: const Icon(Icons.add),
+      appBar: AppBar(
+        title: const Text("분류 수정"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (context) => const CategoryAddWidget()),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
-      body: Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ControlBtnGroup(
-              titleList: const ["수입", "지출"],
-              callbackList: [() {}, () {}],
-            ),
-            Expanded(
-              child: CategoryList(
-                expenseCategories: expenseCategories,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: ColorTheme.cardBackground,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: ColorTheme.cardLabelText, width: 2),
+                ),
+              ),
+              child: TabBar(
+                tabs: _tabs,
+                labelStyle: Theme.of(context).textTheme.titleMedium,
+                controller: _tabController,
+                indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: ColorTheme.expenseColor,
+                  ),
+                ),
+                indicatorSize: TabBarIndicatorSize.tab,
+                labelColor: ColorTheme.cardText,
+                unselectedLabelColor: ColorTheme.cardLabelText,
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                CategoryListWidget(
+                  categories: _expenseCategories,
+                  onReorderCategories: _reorderCategories,
+                  onReorderItems: _reorderItems,
+                  deleteCategory: _deleteCategory,
+                  deleteItem: _deleteItem,
+                ),
+                CategoryListWidget(
+                  categories: _expenseCategories,
+                  onReorderCategories: _reorderCategories,
+                  onReorderItems: _reorderItems,
+                  deleteCategory: _deleteCategory,
+                  deleteItem: _deleteItem,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -73,119 +155,214 @@ class ExpenseCategory {
   ExpenseCategory({required this.category, required this.items});
 }
 
-class CategoryList extends StatelessWidget {
-  final List<ExpenseCategory> expenseCategories;
+class CategoryItem {
+  final String description;
+  final String amount;
 
-  const CategoryList({super.key, required this.expenseCategories});
+  CategoryItem({required this.description, required this.amount});
+}
+
+class CategoryListWidget extends StatelessWidget {
+  final List<ExpenseCategory> categories;
+  final Function(int oldIndex, int newIndex) onReorderCategories;
+  final Function(int categoryIndex, int oldIndex, int newIndex) onReorderItems;
+  final Function(int categoryIndex) deleteCategory;
+  final Function(int categoryIndex, int itemIndex) deleteItem;
+
+  const CategoryListWidget({
+    super.key,
+    required this.categories,
+    required this.onReorderCategories,
+    required this.onReorderItems,
+    required this.deleteCategory,
+    required this.deleteItem,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: expenseCategories.map((category) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Table(
-              columnWidths: const {
-                0: FlexColumnWidth(5),
-                1: FlexColumnWidth(3),
-              },
+    return ReorderableListView(
+      onReorder: onReorderCategories,
+      children: [
+        for (int categoryIndex = 0;
+            categoryIndex < categories.length;
+            categoryIndex++)
+          Container(
+            key: Key('category_$categoryIndex'),
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: ColorTheme.cardBackground,
+              border: Border(
+                bottom: BorderSide(
+                  width: 1.5,
+                  color: Theme.of(context).dividerColor,
+                ),
+              ),
+            ),
+            child: Column(
               children: [
-                TableRow(
+                CategoryCard(
+                  category: categories[categoryIndex].category,
+                  deleteCategory: () {
+                    if (categories[categoryIndex].items.isEmpty) {
+                      deleteCategory(categoryIndex);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          duration: Duration(milliseconds: 1000),
+                          content: Text('Cannot delete category with items'),
+                        ),
+                      );
+                    }
+                  },
+                ),
+                ReorderableListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  onReorder: (int oldIndex, int newIndex) {
+                    onReorderItems(categoryIndex, oldIndex, newIndex);
+                  },
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 32,
-                        right: 16,
-                        top: 8,
-                        bottom: 8,
+                    for (int itemIndex = 0;
+                        itemIndex < categories[categoryIndex].items.length;
+                        itemIndex++)
+                      CategoryItemCard(
+                        key: Key('item_${categoryIndex}_$itemIndex'),
+                        description: categories[categoryIndex]
+                            .items[itemIndex]
+                            .description,
+                        amount:
+                            categories[categoryIndex].items[itemIndex].amount,
+                        deleteItem: () => deleteItem(categoryIndex, itemIndex),
                       ),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: TextField(
-                          controller:
-                              TextEditingController(text: category.category),
-                          style: Theme.of(context).textTheme.titleMedium,
-                          textAlign: TextAlign.left,
-                          decoration:
-                              const InputDecoration(border: InputBorder.none),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 16,
-                        right: 8,
-                        top: 8,
-                        bottom: 8,
-                      ),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: IconButton(
-                          icon: const Icon(
-                            Icons.delete,
-                            color: Colors.redAccent,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                    ),
                   ],
-                )
+                ),
               ],
             ),
-            Table(
-              columnWidths: const {
-                0: FlexColumnWidth(5),
-                1: FlexColumnWidth(3),
-              },
-              children: category.items.map((item) {
-                return TableRow(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 60,
-                        right: 8,
-                        top: 8,
-                        bottom: 8,
-                      ),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: TextField(
-                          controller:
-                              TextEditingController(text: item.description),
-                          style: Theme.of(context).textTheme.bodyMedium,
-                          textAlign: TextAlign.left,
-                          decoration:
-                              const InputDecoration(border: InputBorder.none),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 16,
-                        right: 8,
-                        top: 8,
-                        bottom: 8,
-                      ),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: IconButton(
-                          icon: const Icon(
-                            Icons.delete,
-                            color: Colors.redAccent,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                    ),
-                  ],
-                );
-              }).toList(),
+          ),
+      ],
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String category;
+  final VoidCallback deleteCategory;
+
+  const CategoryCard({
+    super.key,
+    required this.category,
+    required this.deleteCategory,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: ColorTheme.cardBackground,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 0,
+                vertical: 8,
+              ),
+              alignment: Alignment.centerLeft,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
             ),
-            const Divider(),
+            onPressed: () {},
+            child: Text(
+              category,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(color: ColorTheme.cardLabelText),
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            SizedBox(
+              width: 40,
+              height: 40,
+              child: IconButton(
+                icon: const Icon(Icons.delete, size: 20),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(
+              width: 40,
+              height: 40,
+              child: IconButton(
+                icon: const Icon(Icons.drag_indicator, size: 20),
+                onPressed: () {},
+              ),
+            ),
           ],
-        );
-      }).toList(),
+        )
+      ],
+    );
+  }
+}
+
+class CategoryItemCard extends StatelessWidget {
+  final String description;
+  final String amount;
+  final VoidCallback deleteItem;
+
+  const CategoryItemCard({
+    super.key,
+    required this.description,
+    required this.amount,
+    required this.deleteItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: ColorTheme.cardBackground,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 0,
+                vertical: 8,
+              ),
+              alignment: Alignment.centerLeft,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            onPressed: () {},
+            child: Text(
+              description,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            SizedBox(
+              width: 40,
+              height: 40,
+              child: IconButton(
+                icon: const Icon(Icons.delete, size: 20),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(
+              width: 40,
+              height: 40,
+              child: IconButton(
+                icon: const Icon(Icons.drag_indicator, size: 20),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
