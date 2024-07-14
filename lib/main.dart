6096928598ade75bcce38e7325535f680efc8fd2
Jaehoon Song <jhssong02@gmail.com>
2024-07-14 22:52:26 +0900

    feat: Add WillPopScope for back button event (#62)
    WillPopScope가 Deprecated 되었지만 현재 기능적으로 문제가 없음을 확인하였고 PopScope를 사용할 경우 어플 종료 후 재시작 시 _currentBottomNavIndex 값이 초기화되지 않는 버그가 있었음

--- lib/main.dart
+++ lib/main.dart
@@ -36,47 +36,59 @@ class _MyAppState extends State<MyApp> {
     setState(() => _currentBottomNavIndex = index);
   }
 
+  Future<bool> _onWillPop() async {
+    if (_currentBottomNavIndex != 0) {
+      setState(() => _currentBottomNavIndex = 0);
+      return Future.value(false); // Prevent default back action
+    } else {
+      return Future.value(true); // Allow default back action (exit app)
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: theme,
       home: SafeArea(
-        child: Scaffold(
-          bottomNavigationBar: Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(color: ColorTheme.stroke, width: 2),
+        child: WillPopScope(
+          onWillPop: _onWillPop,
+          child: Scaffold(
+            bottomNavigationBar: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(color: ColorTheme.stroke, width: 2),
+                ),
+              ),
+              child: BottomNavigationBar(
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: '홈',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    label: '검색',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.credit_card),
+                    label: '자산',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.bar_chart),
+                    label: '통계',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.menu_rounded),
+                    label: '메뉴',
+                  ),
+                ],
+                currentIndex: _currentBottomNavIndex,
+                onTap: _onBottomNavTapped,
               ),
             ),
-            child: BottomNavigationBar(
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: '홈',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-                  label: '검색',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.credit_card),
-                  label: '자산',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.bar_chart),
-                  label: '통계',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.menu_rounded),
-                  label: '메뉴',
-                ),
-              ],
-              currentIndex: _currentBottomNavIndex,
-              onTap: _onBottomNavTapped,
-            ),
+            body: _screenList.elementAt(_currentBottomNavIndex),
           ),
-          body: _screenList.elementAt(_currentBottomNavIndex),
         ),
       ),
     );
