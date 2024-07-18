5bc0339a8e602c730a5cd8c7bfc621e70a835d05
Bouncyball <80045020+dylangoh235@users.noreply.github.com>
2024-07-18 12:55:04 +0900

    hotfix: Add Key type (#77)

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,8 +3,10 @@ class Constants {
   static const int lastDate = 2200;
 }
 
+typedef Key = String;
+
 class Keys {
-  static const String expenseCategoryKey = "expense_category_key";
-  static const String incomeCategoryKey = "income_category_key";
-  static const String subCategoryKey = "sub_category_key";
+  static const Key expenseCategoryKey = "expense_category_key";
+  static const Key incomeCategoryKey = "income_category_key";
+  static const Key subCategoryKey = "sub_category_key";
 }
